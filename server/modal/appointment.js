a58fdc35d1a2337ca7088b3bd8c6b84049e25ebf
mohit pandey <mht.pdy@gmail.com>
2021-07-05 13:48:51 +0530

    added req headers

--- server/modal/appointment.js
+++ server/modal/appointment.js
@@ -4,8 +4,7 @@ var schema = new mongoose.Schema({
    
     email : {
         type: String,
-        required: true,
-        unique: true
+        required: true
     },
     appointmentDate : {
         type: Date,
