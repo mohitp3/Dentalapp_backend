afcd6f9a4815ce78c76206394642b294dcb4c182
mohit pandey <mht.pdy@gmail.com>
2021-07-17 18:08:07 +0530

    added pagination to appointment modal

--- server/controller/appointments.js
+++ server/controller/appointments.js
@@ -1,107 +1,125 @@
-var Appointment = require('../modal/appointment');
+var Appointment = require("../modal/appointment");
 
 // create and save new appointment
-exports.create = (req,res)=>{
-    // validate request
-    if(!req.body){
-        res.status(400).send({ message : "Content can not be emtpy!"});
-        return;
-    }
+exports.create = (req, res) => {
+  // validate request
+  if (!req.body) {
+    res.status(400).send({ message: "Content can not be emtpy!" });
+    return;
+  }
 
-    // new appointment
-    const appointment = new Appointment({
-        email : req.body.email,
-        appointmentDate: req.body.appointmentDate,
-        message : req.body.message,
-        name : req.body.name
+  // new appointment
+  const appointment = new Appointment({
+    email: req.body.email,
+    appointmentDate: req.body.appointmentDate,
+    message: req.body.message,
+    name: req.body.name,
+  });
+  // save appointment in the database
+  appointment
+    .save(appointment)
+    .then((data) => {
+      res.send(data);
+      // res.redirect('/');
     })
-    // save appointment in the database
-    appointment
-        .save(appointment)
-        .then(data => {
-            res.send(data);
-            // res.redirect('/');
-        })
-        .catch(err =>{
-            res.status(500).send({
-                message : err.message || "Something went wrong"
-            });
-        });
-
-}
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Something went wrong",
+      });
+    });
+};
 //get appointments
-exports.find = (req, res)=>{
-    if(req.params.id){
-        const id = req.params.id;
-
-        Appointment.findById(id)
-            .then(data =>{
-                if(!data){
-                    res.status(404).send({ message : "Not found appointment with id "+ id})
-                }else{
-                    res.send(data)
-                }
-            })
-            .catch(err =>{
-                res.status(500).send({ message: "Erro retrieving appointment with id " + id})
-            })
-
-    }else{
-        Appointment.find()
-            .then(appointment => {
-                res.send(appointment)
-            })
-            .catch(err => {
-                res.status(500).send({ message : err.message || "Error Occurred while retriving appointment information" })
-            })
-    }
-
-    
-}
-// Update a new idetified appointment by appointment id
-exports.update = (req, res)=>{
-    if(!req.body){
-        return res
-            .status(400)
-            .send({ message : "Data to update can not be empty"})
-    }
-    if(!req.params && !req.params.id){
-        return res
-            .status(400)
-            .send({ message : "Provide ID to update"})
-    }
+exports.find = (req, res) => {
+  if (req.params.id) {
     const id = req.params.id;
-    Appointment
-    .findByIdAndUpdate(id, req.body, { useFindAndModify: false,new:true })
-        .then(data => {
-            if(!data){
-                res.status(404).send({ message : `Cannot Update appointment with ${id}. Maybe appointment not found!`})
-            }else{
-                res.send(data)
-            }
-        })
-        .catch(err =>{
-            res.status(500).send({ message : "Error Update appointment information"})
-        })
-}
+    Appointment.findById(id)
+      .then((data) => {
+        if (!data) {
+          res
+            .status(404)
+            .send({ message: "Not found appointment with id " + id });
+        } else {
+          res.send(data);
+        }
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .send({ message: "Erro retrieving appointment with id " + id });
+      });
+  } else {
+    const { page = 1, limit = 5 } = req.query;
+    const data = {
+      total: 0,
+      current: 0,
+      appointments: [],
+    };
+    Promise.all([
+      Appointment.find()
+        .countDocuments()
+        .then((count) => {
+          data.total = count;
+        }),
+      Appointment.find()
+        .limit(limit * 1)
+        .skip((page - 1) * limit)
+        .then((appointment) => {
+          data.current = appointment.length;
+          data.appointments = appointment;
+        }),
+    ])
+      .then((result) => {
+        res.send(data);
+      })
+      .catch((err) => res.status(500).send({ message: err.message }));
+  }
+};
+// Update a new idetified appointment by appointment id
+exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({ message: "Data to update can not be empty" });
+  }
+  if (!req.params && !req.params.id) {
+    return res.status(400).send({ message: "Provide ID to update" });
+  }
+  const id = req.params.id;
+  Appointment.findByIdAndUpdate(id, req.body, {
+    useFindAndModify: false,
+    new: true,
+  })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot Update appointment with ${id}. Maybe appointment not found!`,
+        });
+      } else {
+        res.send(data);
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({ message: "Error Update appointment information" });
+    });
+};
 
 // Delete a appointment with specified appointment id in the request
-exports.delete = (req, res)=>{
-    const id = req.params.id;
+exports.delete = (req, res) => {
+  const id = req.params.id;
 
-    Appointment.findByIdAndDelete(id)
-        .then(data => {
-            if(!data){
-                res.status(404).send({ message : `Cannot Delete with id ${id}. Maybe id is wrong`})
-            }else{
-                res.send({
-                    message : "appointment was deleted successfully!"
-                })
-            }
-        })
-        .catch(err =>{
-            res.status(500).send({
-                message: "Could not delete appointment with id=" + id
-            });
+  Appointment.findByIdAndDelete(id)
+    .then((data) => {
+      if (!data) {
+        res
+          .status(404)
+          .send({ message: `Cannot Delete with id ${id}. Maybe id is wrong` });
+      } else {
+        res.send({
+          message: "appointment was deleted successfully!",
         });
-}
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Could not delete appointment with id=" + id,
+      });
+    });
+};
